fc009d8014284af0781d9b5e6fd99f0d2c9375f5
Huoyubai-hy <1943334878@qq.com>
2020-12-24 09:06:29 -0500

    Lab1 modify

--- user/find.c
+++ user/find.c
@@ -6,20 +6,12 @@
 char*
 fmtname(char *path)
 {
-  static char buf[DIRSIZ+1];
   char *p;
-
-  // Find first character after last slash.
   for(p=path+strlen(path); p >= path && *p != '/'; p--)
     ;
   p++;
 
-  // Return blank-padded name.
-  if(strlen(p) >= DIRSIZ)
-    return p;
-  memmove(buf, p, strlen(p));
-  memset(buf+strlen(p), 0, DIRSIZ-strlen(p));
-  return buf;
+  return p;
 }
 
 void
@@ -43,13 +35,12 @@ find(char *path,char *name)
 
   switch(st.type){
   case T_FILE:
-    // printf("%s %d %d %l\n", fmtname(path), st.type, st.ino, st.size);
     if(strcmp(fmtname(path),name)==0)
         printf("%s\n",path);
     break;
 
   case T_DIR:
-    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+    if(strlen(path) + 1 + DIRSIZ + 1> sizeof(buf)){
       printf("ls: path too long\n");
       break;
     }
@@ -63,11 +54,6 @@ find(char *path,char *name)
         continue;
       memmove(p, de.name, DIRSIZ);
       p[DIRSIZ] = 0;
-    //   if(stat(buf, &st) < 0){
-    //     printf("ls: cannot stat %s\n", buf);
-    //     continue;
-    //   }
-    //   printf("%s %d %d %d\n", fmtname(buf), st.type, st.ino, st.size);
       find(buf,name);
     }
     break;
